d00d2e0aedbe7cb865fb9a94a3a76ed0258aacf7
Brian Luc <brian.luc@smash.org.au>
2018-02-27 22:28:00 +1100

    Added basic frontend layout

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
+
 export class HomePage {
+	search: FormGroup;
+	cuisine: string;
+	radius: number;
+	isSearch: boolean = false;
+	
+	constructor(public navCtrl: NavController,
+				public formBuilder: FormBuilder) {
+		this.search = formBuilder.group({
+			cuisine: [''],
+			radius: [''],
+		});
+	}
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+	ionViewDidLoad(){
+		console.log("Executing command on startup");
+	}
+	
+	searchRestaurants(){
+		console.log("Searching For Restaurants");
+		let radius = this.search.controls['radius'].value;
+		if(!radius){
+			radius = 100;
+		}
+		console.log(radius);
+		this.isSearch = true;
+	}
+	
 }
